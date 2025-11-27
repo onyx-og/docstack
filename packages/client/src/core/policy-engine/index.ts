ba2abcb2a96a6a9028aeeb1a963e3d8ba1c79b52
Onyx <onyx.og@outlook.com>
2025-11-27 20:52:34 +0100

    Remove isolated-vm from policy evaluation

--- packages/client/src/core/policy-engine/index.ts
+++ packages/client/src/core/policy-engine/index.ts
@@ -1,63 +1,5 @@
-import vm from "vm";
 import type ClientStack from "../stack.js";
 import type { AuthSessionProof, Document, PolicyModel } from "@docstack/shared";
-
-type IsolatedVmLike = typeof import("isolated-vm");
-
-const createFallbackIsolate = (): IsolatedVmLike => {
-    class ExternalCopy<T> {
-        private readonly value: T;
-        constructor(value: T) {
-            this.value = value;
-        }
-        copyInto() {
-            return this.value;
-        }
-    }
-
-    class FallbackGlobal {
-        [key: string]: any;
-        async set(key: string, value: any) {
-            this[key] = value;
-        }
-        derefInto() {
-            return this;
-        }
-    }
-
-    class FallbackContext {
-        public global: FallbackGlobal;
-        constructor() {
-            this.global = new FallbackGlobal();
-        }
-        async eval(code: string) {
-            return vm.runInNewContext(code, this.global);
-        }
-    }
-
-    class Isolate {
-        async createContext() {
-            return new FallbackContext();
-        }
-        dispose() {
-            return undefined;
-        }
-    }
-
-    return {
-        Isolate,
-        ExternalCopy,
-    } as unknown as IsolatedVmLike;
-};
-
-const ivm: IsolatedVmLike = (() => {
-    try {
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        return require("isolated-vm");
-    } catch (error) {
-        return createFallbackIsolate();
-    }
-})();
 
 const SYSTEM_CLASSES = new Set([
     "~Policy",
@@ -105,20 +47,18 @@ export class PolicyEngine {
     }
 
     private async evaluateRule(policy: PolicyModel, document: Document | null, session: AuthSessionProof): Promise<boolean> {
-        const isolate = new ivm.Isolate({ memoryLimit: 8 });
-        const context = await isolate.createContext();
-        const jail = context.global;
-        await jail.set("global", jail.derefInto());
-        await jail.set("document", new ivm.ExternalCopy(document || {}).copyInto());
-        await jail.set("session", new ivm.ExternalCopy(session.session).copyInto());
+        const executor = new Function(
+            "document",
+            "session",
+            `"use strict"; ${policy.rule}`
+        ) as (doc: Document, sess: AuthSessionProof["session"]) => any;
 
-        const wrapped = `(() => { ${policy.rule}; })()`;
-        try {
-            const result = await context.eval(wrapped, { promise: true });
-            return Boolean(result);
-        } finally {
-            isolate.dispose();
+        const result = executor(document || {}, session.session);
+        if (result instanceof Promise) {
+            return Boolean(await result);
         }
+
+        return Boolean(result);
     }
 
     private async authorize(targetClass: string, operation: PolicyOperation, document: Document | null): Promise<boolean> {
