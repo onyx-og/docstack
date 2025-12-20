1e37ddab2b3d5022678884a90db6126dbd1feb33
Onyx <hello@onyx.ac>
2025-12-20 11:29:14 +0000

    Bundle shared package

--- packages/client/rollup.config.js
+++ packages/client/rollup.config.js
@@ -1,5 +1,7 @@
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: './src/index.ts',
@@ -24,6 +26,10 @@ export default {
     // }
   ],
   plugins: [
+    resolve({
+      preferBuiltins: false
+    }),
+    commonjs(),
     json(),
     typescript()
   ]
